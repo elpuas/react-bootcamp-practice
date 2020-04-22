cb1bf05dabc731d12546d697d5bd66e8597fc9db
Alfredo Navas <elpuas@gmail.com>
2020-04-22 15:29:53 -0600

    Chreate Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        APP HERE
+        <Header />
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => (
+	<header className="App-Header">
+		<ul className="container">
+			<li>SiteTitle</li>
+		</ul>
+	</header>
+)
+
+export default Header;
