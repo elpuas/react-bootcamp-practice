ca439fe72f226799c9524a73394f9d1517e8d6ed
Alfredo Navas <elpuas@gmail.com>
2020-04-27 07:20:43 -0600

    add header and posts

--- src/App.js
+++ src/App.js
@@ -4,10 +4,32 @@ import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
+  // Load some posts into state.
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Hello React",
+        content: "Lorem."
+      },
+      {
+        id: 2,
+        title: "Hello Project",
+        content: "Tothe."
+      },
+      {
+        id: 3,
+        title: "Hello Blog",
+        content: "Ipsum."
+      }
+    ]
+  };
+
   render(){
     return(
       <div className="App">
         <Header />
+        <Post posts={ this.state.posts }/>
       </div>
     )
   }
