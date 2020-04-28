20799b7d6fff43824d457d9b52f07bce3b76a1ff
Alfredo Navas <elpuas@gmail.com>
2020-04-27 18:52:54 -0600

    add quill

--- src/App.js
+++ src/App.js
@@ -4,33 +4,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/Header';
 import Posts from './components/Posts';
 import Post from './components/Post';
-import NotFound from './components/NotFound'
+import NotFound from './components/NotFound';
+import PostForm from './components/PostForm';
 
 import './tailwind.generated.css';
 
 class App extends Component {
   // Load some posts into state.
   state = {
-    posts: [
-      {
-        id: 1,
-        title: "Hello React",
-        slug:"hello-react",
-        content: "Lorem."
-      },
-      {
-        id: 2,
-        title: "Hello Project",
-        slug:"hello-project",
-        content: "Tothe."
-      },
-      {
-        id: 3,
-        title: "Hello Blog",
-        slug:"hello-blog",
-        content: "Ipsum."
-      }
-    ]
+    posts: []
+  };
+  addNewPost = post => {
+    post.id = this.state.posts.length + 1;
+    post.slug = encodeURIComponent(
+      post.title
+        .toLowerCase()
+        .split(" ")
+        .join("-")
+    );
+    this.setState({
+      posts: [...this.state.posts, post]
+    });
   };
 
   render(){
@@ -44,7 +38,9 @@ class App extends Component {
             path="/"
             render={() => <Posts posts={this.state.posts} /> }
             />
-            <Route path="/post/:postSlug" render={props => {
+            <Route
+            path="/post/:postSlug"
+            render={props => {
               const post = this.state.posts.find(
                 post => post.slug === props.match.params.postSlug
               );
@@ -52,7 +48,13 @@ class App extends Component {
               }
             }
             />
-            <Route component={NotFound} />
+            <Route
+            exact
+            path="/new"
+            render={() => <PostForm addNewPost={this.addNewPost} />}
+            />
+            <Route
+            component={NotFound} />
         </Switch>
       </div>
       </Router>
